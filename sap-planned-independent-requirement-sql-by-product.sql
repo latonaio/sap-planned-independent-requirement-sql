e18c04921227771121db8350fa99ac8c2b2f0e7a
Yurino Oga <yurino.o@latona.io>
2021-11-24 10:53:23 +0900

    Merge branch 'main' of github.com:latonaio/sap-planned-independent-requirement-sql into main

--- sap-planned-independent-requirement-sql-by-product.sql
+++ sap-planned-independent-requirement-sql-by-product.sql
@@ -2,16 +2,17 @@ CREATE TABLE `sap-planned-independent-requirement-by-product`
 (
     `Product`               varchar(40) DEFAULT NULL,
     `Plant`                 varchar(4) DEFAULT NULL,
+    `MRPArea`               varchar(10) DEFAULT NULL,
     `PlndIndepRqmtType`     varchar(4) DEFAULT NULL,
     `PlndIndepRqmtVersion`  varchar(2) DEFAULT NULL,    
     `RequirementPlan`       varchar(10) DEFAULT NULL,
+    `RequirementSegment`    varchar(40) DEFAULT NULL,
     `PlndIndepRqmtInternalID` int(12) DEFAULT NULL,
-    `MRPArea`               varchar(10) DEFAULT NULL,
     `UnitOfMeasure`         varchar(3) DEFAULT NULL,
     `PlndIndepRqmtPeriod`   varchar(3) DEFAULT NULL,
     `PlndIndepRqmtPeriodStartDate` date DEFAULT NULL,
     `PlannedQuantity`       float(13) DEFAULT NULL,
     `LastChangeDate`        date DEFAULT NULL,
-    PRIMARY KEY (`Product`, `Plant`, `PlndIndepRqmtType`, `PlndIndepRqmtVersion`, `RequirementPlan`)
+    PRIMARY KEY (`Product`, `Plant`, `MRPArea`, `PlndIndepRqmtType`, `PlndIndepRqmtVersion`, `RequirementPlan`, `RequirementSegment`)
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8mb4;
