bad008455c5af3f948bb1931c1d8797db02170ef
Yurino Oga <yurino.o@latona.io>
2021-11-28 13:19:54 +0900

    release

--- sap-planned-independent-requirement-sql-by-product.sql
+++ sap-planned-independent-requirement-sql-by-product.sql
@@ -7,11 +7,11 @@ CREATE TABLE `sap-planned-independent-requirement-by-product`
     `PlndIndepRqmtVersion`  varchar(2) DEFAULT NULL,    
     `RequirementPlan`       varchar(10) DEFAULT NULL,
     `RequirementSegment`    varchar(40) DEFAULT NULL,
-    `PlndIndepRqmtInternalID` int(12) DEFAULT NULL,
+    `PlndIndepRqmtInternalID` varchar(12) DEFAULT NULL,
     `UnitOfMeasure`         varchar(3) DEFAULT NULL,
     `PlndIndepRqmtPeriod`   varchar(3) DEFAULT NULL,
     `PlndIndepRqmtPeriodStartDate` date DEFAULT NULL,
-    `PlannedQuantity`       float(13) DEFAULT NULL,
+    `PlannedQuantity`       varchar(13) DEFAULT NULL,
     `LastChangeDate`        date DEFAULT NULL,
     PRIMARY KEY (`Product`, `Plant`, `MRPArea`, `PlndIndepRqmtType`, `PlndIndepRqmtVersion`, `RequirementPlan`, `RequirementSegment`)
 ) ENGINE = InnoDB
